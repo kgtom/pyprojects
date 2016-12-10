31ca074372f77115d1d7e95f705272cf4d5deee4
tom <leiweizhen@zailushang.cn>
2016-12-10 14:56:55 +0800

    add  py27

--- Demo1/hello.py
+++ Demo1/hello.py
@@ -12,3 +12,36 @@ if True:
 else:
     print ("b")
 '''
+
+
+#input("\n\nPress the enter key to exit.")
+counter=100
+mile=10.0
+name="tom"
+
+print (counter)  
+print (mile) 
+print (name)
+
+str = 'Hello World!'
+print(str)
+print(str[1:2])
+print counter
+print str[0]
+print str+"test"
+
+list=["tom",123,3.34,]
+tinyList=[1234,'john']
+
+print list
+print list[0]
+print tinyList*2
+print list +tinyList
+
+for ele in list:
+ print (ele)
+
+ name1=raw_input()
+ 
+
+
