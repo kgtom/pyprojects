587ca701bee36e6c2f7d158cc3683b8ec3363341
tom <leiweizhen@zailushang.cn>
2016-12-26 21:11:12 +0800

    add l返回函数

--- Demo1/.vscode/highDef.py
+++ Demo1/.vscode/highDef.py
@@ -5,4 +5,56 @@ print filter(is_odd,[1,2,3,5])
 
 print sorted([36, 5, 12, 9, 21])
 
+def lazy_sum(*args):
+    def sum():
+        ax = 0
+        for n in args:
+            ax = ax + n
+        return ax
+    return sum
+f = lazy_sum(1, 3, 5, 7, 9)
+print f
+print f()
+for i in range(1,4):
+    print i
+#返回函数  闭包1
+def count():
+     fs=[]
+     for i in range(1,4):
+         def f():
+             return i*i
+         fs.append(f)
+     return fs
+f1,f2,f3=count()
+print f1
+print f1()
+print f2
+print f2()
+print f3                
+print f3()
 
+#返回函数  闭包2
+def count():
+     fs=[]
+     for i in range(1,4):
+         def f(j=i):
+             return j*j
+         fs.append(f)
+     return fs
+f1,f2,f3=count()
+print f1
+print f1()
+print f2
+print f2()
+print f3                
+print f3()
+
+#匿名函数
+print map(lambda x: x * x, [1, 2, 3])
+
+#装饰器
+import datetime
+def now():
+    return datetime.datetime.now()    
+f=now
+print f()
